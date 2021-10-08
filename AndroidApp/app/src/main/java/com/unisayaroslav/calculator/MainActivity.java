8741b71f47b6bb72103317b6c3ac02bcbac16d36
TYaroslavit <92084622+TYaroslavit@users.noreply.github.com>
2021-10-09 01:53:45 +0200

    Rewrited v1.1

--- AndroidApp/app/src/main/java/com/unisayaroslav/calculator/MainActivity.java
+++ AndroidApp/app/src/main/java/com/unisayaroslav/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,25 +16,30 @@ import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     int num;
-    int[] result = new int[16];
-    String numero = "", numIns;
+    String numero = "";
     EditText mEdit;
     TextView text1;
 
 
-    private int[]  calcolabase(int base, int B1) {
-        int[] arr = new int[16];
-        int p = 15;
+    private String calcolabase(int base, int B1) {
+        int size;
+        if (base == 2) size = 27;
+        else if (base == 3) size = 17;
+        else size = 14;
+        int[] arr = new int[size];
+        int p = size - 1;
         while (B1 >= base) {
             if (B1 % base == 0) arr[p] = 0;
             else arr[p] = 1;
             B1 = B1 / base;
             p--;
-
         }
         if (B1 == base) arr[p] = 1;
         else arr[p] = B1;
-        return arr;
+        for (int i = 0; i < size; i++) {
+            numero = numero.concat(Integer.toString(arr[i]));
+        }
+        return numero;
         /*
         for (int i = 0; i < 5; i++) {
             System.out.println("valori " + result[i]);
@@ -51,138 +57,44 @@ public class MainActivity extends AppCompatActivity {
         text1 = (TextView) findViewById(R.id.result);
     }
 
-    /*public void CambioSfondo(View v) {
-        int r =rand1.nextInt( 255)+1;
-        int g = rand1.nextInt( 255)+1;
-        int b = rand1.nextInt( 255)+1;
-        v.setBackgroundColor(Color.rgb(r,g,b));
-    }*/
-    public void base2(View a) {
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
+    public void DigitPress(View a) {
+        int base;
+        num = Integer.valueOf(mEdit.getText().toString());
+        numero = "";
+        Button b = (Button) a;
+        int id = b.getId();
+        switch (id) {
+            case R.id.Button1:
+                text1.setText(calcolabase(2, num).toString());
+                break;
+            case R.id.Button2:
+                text1.setText(calcolabase(3, num).toString());
+                break;
+            case R.id.Button3:
+                text1.setText(calcolabase(4, num).toString());
+                break;
+            case R.id.Button4:
+                text1.setText(calcolabase(5, num).toString());
+                break;
+            case R.id.Button5:
+                text1.setText(calcolabase(6, num).toString());
+                break;
+            case R.id.Button6:
+                text1.setText(calcolabase(7, num).toString());
+                break;
+            case R.id.Button7:
+                text1.setText(calcolabase(8, num).toString());
+                break;
+            case R.id.Button8:
+                text1.setText(calcolabase(9, num).toString());
+                break;
+            case R.id.Button10:
+                text1.setText(calcolabase(16, num).toString());
+                break;
         }
-        result=calcolabase(2, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
 
-    public void base3(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(3, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-
     }
-    public void base4(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(4, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    public void base5(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(5, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    public void base6(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(6, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    public void base7(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(7, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    public void base8(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(8, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    public void base9(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(9, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-    /*
-    public void base10(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(10, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    } */
-    public void base16(View a){
-        num = 0;
-        numIns = mEdit.getText().toString();
-        for (int i = 0; i < numIns.length(); i++) {
-            num = num * 10 + Character.getNumericValue(numIns.charAt(i));
-        }
-        result=calcolabase(16, num);
-        for (int i = 0; i < 16; i++) {
-            numero=numero.concat( Integer.toString(result[i]));
-        }
-        text1.setText(numero);
-    }
-
-
-    public void Reset(View a){
+    public void Reset(View a) {
         numero = "";
         text1.setText(numero);
     }
